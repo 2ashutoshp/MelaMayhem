915b0becec0692362edbe78384ce959794326f98
2ashutoshp <2ashutoshp@gmail.com>
2024-07-06 13:33:46 -0400

    debugging mongo connection

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,16 +1,21 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI ?? '';
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
-  if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'mela-mayhem',
-    bufferCommands: false,
-  })
-  cached.conn = await cached.promise;
-  return cached.conn;
+  try {
+    if (cached.conn) return cached.conn;
+    if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+      dbName: 'mela-mayhem',
+      bufferCommands: false,
+    })
+    cached.conn = await cached.promise;
+    return cached.conn;
+  } catch (error) {
+    console.error('Error connecting to MongoDB: ', (error as Error).message);
+    throw error;
+  }
 };
